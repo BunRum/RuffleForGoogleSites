b99bbd1cecfc26c370863f65a539653a8e776b5a
BunRum <61174957+BunRum@users.noreply.github.com>
2022-02-06 00:58:10 -0800

    Update launch.js

--- launch.js
+++ launch.js
@@ -1,18 +1,20 @@
 loadScript("https://bunrum.github.io/RuffleForGoogleSites/ruffle.js")
-window.RufflePlayer = window.RufflePlayer || {};
-window.addEventListener("load", (event) => {
-    const ruffle = window.RufflePlayer.newest();
-    const player = ruffle.createPlayer();
-    const container = document.getElementById("container");
-    container.appendChild(player);
-	player.Config = {
-		"backgroundColor": "#000000",
-		"contextMenu": true,
-	        "autoplay": "auto",
-	        "letterbox": "on",
-		"unmuteOverlay": "visible",
-	};
-    player.load("https://bunrum.github.io/RuffleForGoogleSites/portal.swf");
-    player.style.width = "100%"; 
-    player.style.height = "100%";
-});
+    .then(function(script) {
+        window.RufflePlayer = window.RufflePlayer || {};
+        window.addEventListener("load", (event) => {
+            const ruffle = window.RufflePlayer.newest();
+            const player = ruffle.createPlayer();
+            const container = document.getElementById("container");
+            container.appendChild(player);
+            player.Config = {
+                "backgroundColor": "#000000",
+                "contextMenu": true,
+                    "autoplay": "auto",
+                    "letterbox": "on",
+                "unmuteOverlay": "visible",
+            };
+            player.load("https://bunrum.github.io/RuffleForGoogleSites/portal.swf");
+            player.style.width = "100%"; 
+            player.style.height = "100%";
+        });
+    })        
